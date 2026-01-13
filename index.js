d8e7f8d529c08af0661d4aa8d18bdc50c0e118e3
Vaishnavi <Vaishnavi09.099@gmail.com>
2026-01-13 16:58:10 +0530

    Added CRUD apis for users

--- index.js
+++ index.js
@@ -68,7 +68,152 @@ app.delete("/blogs/:id",(req,res)=>{
 })
 
 
+
+
+
+let users = [];
+app.post("/users",(req,res)=>{
+    const{name,password,email }=req.body;
+    try{
+        if(!name){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill the name",
+            });
+        }
+        if(!password){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill the password"
+            })
+        }
+        if(!email){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill the email",
+            })
+        }
+        users.push({...req.body,id:users.length+1});
+        return res.status(200).json({
+            success:true,
+            message:"user created successfully",
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"Please try again",
+        })
+
+    }
+})
+
+app.get("/users",(req,res)=>{
+    try{
+        return res.status(200).json({
+            success:true,
+            message:"Users fetched successfully",
+            users
+        })
+
+    }
+    catch(err){
+          return res.status(500).json({
+            success:false,
+            message:"Please try again",
+        })
+
+    }
+})
+
+app.get("/users/:id",(req,res)=>{
+    try{
+        const user = users.filter((user)=>user.id == req.params.id)
+        if(!user){
+             return res.status(404).json({
+            success:false,
+            message:"User not found",
+            
+        })
+            
+        }
+        return res.json({
+            success:true,
+            message:"User fetched successfully",
+            user,
+
+        })
+       
+
+    }
+    catch(err){
+          return res.status(500).json({
+            success:false,
+            message:"Cannot find User",
+        })
+
+    }
+})
+
+app.put("/users/:id",(req,res)=>{
+    
+    
+     try{
+        const {id} = req.params
+        const index = users.findIndex(user => user.id == id)
+        if(index==-1){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+
+        users[index]={...users[index],...req.body}
+        return res.status(200).json({
+            message:"user updated successfully",
+        user:users[index],
+    })
+
+    }
+    catch(err){
+        return res.status(500).json({message : "user not found"})
+
+    }
+})
+
+app.delete("/users/:id",(req,res)=>{
+    try{
+
+           const {id} = req.params
+        const index = users.findIndex(user => user.id == id)
+        if(index==-1){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        users = users.filter((user)=>user.id != req.params.id)
+        
+             return res.status(200).json({
+            success:false,
+            message:"User deleted successfully",
+            users,
+        })
+       
+
+    }
+    catch(err){
+          return res.status(500).json({
+            success:false,
+            message:"error deleting user",
+        })
+
+    }
+})
+
+
+
 app.listen(3000,()=>{
     console.log("server started");
 })
-
